02dbeba36320edef05f9ad4009c08793136cc1c4
Matthias Brettschneider <frosch03@gmail.com>
2012-08-17 23:30:56 +0200

    some more stuff

--- Drone.hs
+++ Drone.hs
@@ -1,10 +1,12 @@
 module Drone 
-    ( TnC, Telemetry, Command, Killswitch
+    ( TnC, Telemetry, Command, Killswitch, MVTele, MVCmd, MVTnC
     , hatchDrone
+    , hatchDrone_
     )
 where
 
 import Data.List
+import Data.Sequence
 import Text.Printf
 
 import Network
@@ -19,6 +21,7 @@ import System.IO
 import System.Exit
 
 import Prelude hiding (catch)
+import qualified Prelude as P
 
 --Intern 
 import Message
@@ -33,36 +36,49 @@ nick   = "testProg"
 toMsg  = msg nick (Left chan)
 
 
-type Telemetry  = MVar Message
-type Command    = MVar Message
+type Telemetry  = (Seq Message)
+type Command    = Message
 type TnC        = (Telemetry, Command)
+type MVTele     = MVar Telemetry
+type MVCmd      = MVar Command
+type MVTnC      = (MVTele, MVCmd)
 type Killswitch = MVar Bool
 
 main :: IO ()
 main 
-    = do mvMsg  <- newMVar emptyMsg
-         mvBack <- newMVar emptyMsg
+    = do mvCmd  <- newMVar emptyMsg
+         mvTel  <- newMVar Data.Sequence.empty
          mvQuit <- newMVar False
-         forkIO (hatchDrone "test" "#test" (mvMsg, mvBack) mvQuit)
-         forkIO (tester mvMsg mvQuit)
-         forever mvQuit $ do s <- tryTakeMVar mvBack
-                             maybe (return ()) (putStrLn . show) s
+         forkIO (hatchDrone "test" "#test" (mvTel, mvCmd) mvQuit)
+         forkIO (tester mvCmd mvQuit)
+         forever mvQuit $ do s <- tryTakeMVar mvTel 
+                             maybe (return ()) 
+                                   (\t -> sequence_ $ map putStrLn $ showTelemetry t) 
+                                   $ s
     where forever mvQuit a = readMVar mvQuit >>= cond (return ())
                                                       (a >> forever mvQuit a)
 
-tester :: Command -> Killswitch -> IO ()
-tester mvMsg  mvQuit
+showTelemetry :: Telemetry -> [String]
+showTelemetry t = showTel_ (viewr t) [] 
+
+showTel_ :: ViewR Message -> [String] -> [String]
+showTel_ EmptyR s = s
+showTel_ (ts :> t) s = showTel_ (viewr ts) $ (show t):s
+
+
+tester :: MVCmd -> Killswitch -> IO ()
+tester mvCmd  mvQuit
     = forever
     $ do x <- getLine
          if x == "quit" then swapMVar mvQuit True >> return ()
-                        else putMVar  mvMsg (toMsg x)
+                        else putMVar  mvCmd (toMsg x)
     where forever a  = readMVar mvQuit >>= cond (return ())
                                                 (a >> forever a)
                                                 
 
 
-hatchDrone :: Nick -> Channel -> TnC -> Killswitch -> IO ()
-hatchDrone nick chan (mvMsg, mvBack) mvQuit
+hatchDrone :: Nick -> Channel -> MVTnC -> Killswitch -> IO ()
+hatchDrone nick chan (mvTel, mvCmd) mvQuit
     = bracket (ircConnect server port) disconnect runDrone
     where disconnect  = hClose . socket
           runDrone st = runReaderT drone st
@@ -70,44 +86,70 @@ hatchDrone nick chan (mvMsg, mvBack) mvQuit
                            ircJoinChannel  chan
                            ircPrivmsg      (toMsg "hello world ...")
                            h <- asks socket
-                           ircIO $ forkIO (readDrone mvBack mvQuit h)
+                           ircIO $ forkIO (readDrone mvTel  mvQuit h)
                            forever droneLoop
           toMsg       = msg nick (Left chan)
-          droneLoop   = do msg <- ircIO $ tryTakeMVar mvMsg
+          droneLoop   = do msg <- ircIO $ tryTakeMVar mvCmd
                            maybe (return ()) ircPrivmsg msg
           forever a  = ircIO (readMVar mvQuit) >>= cond (a >> ircPrivmsg (toMsg "by") >> return ())
                                                         (a >> forever a)
                                                           
 
-readDrone :: Telemetry -> Killswitch -> Handle -> IO ()
-readDrone mvBack mvQuit h = forever
+readDrone :: MVTele -> Killswitch -> Handle -> IO ()
+readDrone mvTel  mvQuit h = forever
     $ do s <- init `fmap` (hGetLine h)
-         putMVar mvBack $ parsePRIVMSG s
+         isEmptyMVar mvTel 
+            >>= cond 
+                 (putMVar     mvTel (singleton $ parsePRIVMSG s))
+                 (modifyMVar_ mvTel (\t -> return ((parsePRIVMSG s) <| t)))
          if ping s then pong s else (return ())
     where forever a  = readMVar mvQuit >>= cond (return ())
                                                 (a >> forever a)
           ping x    = "PING :" `isPrefixOf` x
-          pong x    = runReaderT (ircWrite "PONG" (':' : drop 6 x)) (Client h)
+          pong x    = runReaderT (ircWrite "PONG" (':' : P.drop 6 x)) (Client h)
 
 parsePRIVMSG :: String -> Message
 parsePRIVMSG s = Msg nick (Left chan) msg
-    where (nick, s')    = allUntil '!' . drop 1 $ s 
-          (host, s'')   = allUntil ' ' . drop 1 $ s'
-          (_   , s''')  = allUntil ' ' . drop 1 $ s''
-          (chan, s'''') = allUntil ' ' . drop 1 $ s'''
-          msg           = drop 2 $ s''''
+    where (nick, s')    = allUntil '!' . P.drop 1 $ s 
+          (host, s'')   = allUntil ' ' . P.drop 1 $ s'
+          (_   , s''')  = allUntil ' ' . P.drop 1 $ s''
+          (chan, s'''') = allUntil ' ' . P.drop 1 $ s'''
+          msg           = P.drop 2 $ s''''
           allUntil c = (\x -> (takeWhile (/= c) x, dropWhile (/= c) x))
 
 
 
 
-post2IRC :: Message -> IO ()
-post2IRC _ = undefined
+-- Dummy-drone for testing another function
+-- (this one is supposed to echo everything)
+hatchDrone_ :: Nick -> Channel -> MVTnC -> Killswitch -> IO ()
+hatchDrone_ nick chan (mvTel, mvCmd) mvQuit
+    = bracket (ircConnect server port) disconnect runDrone
+    where disconnect  = hClose . socket
+          runDrone st = runReaderT drone st
+          drone       = do ircRegisterNick nick
+                           ircJoinChannel  chan
+                           ircPrivmsg      (toMsg "hello world ...")
+                           h <- asks socket
+                           ircIO $ forkIO (readDrone mvTel  mvQuit h)
+                           forever droneLoop
+          toMsg       = msg nick (Left chan)
+          droneLoop   = do msg <- ircIO $ tryTakeMVar mvCmd
+                           maybe (return ()) ircPrivmsg msg
+          forever a  = ircIO (readMVar mvQuit) >>= cond (a >> ircPrivmsg (toMsg "by") >> return ())
+                                                        (a >> forever a)
 
-isIntressting :: Message -> Bool
-isIntressting _ = undefined
-
-notifyUser :: IO ()
-notifyUser = undefined
-
+readDrone_ :: MVTele -> Killswitch -> Handle -> IO ()
+readDrone_ mvTel  mvQuit h = forever
+    $ do s <- init `fmap` (hGetLine h)
+         runReaderT (return $ ircPrivmsg (parsePRIVMSG s)) (Client h)
+         isEmptyMVar mvTel 
+            >>= cond 
+                 (putMVar     mvTel (singleton $ parsePRIVMSG s))
+                 (modifyMVar_ mvTel (\t -> return ((parsePRIVMSG s) <| t)))
+         if ping s then pong s else (return ())
+    where forever a  = readMVar mvQuit >>= cond (return ())
+                                                (a >> forever a)
+          ping x    = "PING :" `isPrefixOf` x
+          pong x    = runReaderT (ircWrite "PONG" (':' : P.drop 6 x)) (Client h)
 
--- Nest.hs
+++ Nest.hs
@@ -1,6 +1,8 @@
 module Nest 
 where
 
+import Data.Sequence
+import Data.Foldable (toList)
 import Data.HashMap
 import Data.ByteString.Char8
 import Data.Digest.SHA256
@@ -17,12 +19,13 @@ import Prelude hiding (lookup)
 -- Intern
 import Message 
 import Drone
+import Cond
 
 chan = "#test"
 
 data Drone 
-    = Drone { telemetry  :: Telemetry
-            , command    :: Command
+    = Drone { telemetry  :: MVTele
+            , command    :: MVCmd
             , killswitch :: Killswitch
             }
 
@@ -41,38 +44,64 @@ newNest = Nest (Data.HashMap.empty)
 
 newDrone :: Nick -> IO (Nick, Drone)
 newDrone nick 
-    = do mvTel  <- newMVar emptyMsg
+    = do mvTel  <- newMVar Data.Sequence.empty
          mvCmd  <- newMVar emptyMsg
          mvQuit <- newMVar False
          return (nick, (Drone mvTel mvCmd mvQuit))
 
-addDrone :: (Nick, Drone) -> NestM ()
-addDrone (nick, drone) 
-    = do d    <- drones.get
-         chan <- channel.get
-         forkIO (hatchDrone nick chan (mvCmd, mvTel) mvQuit) 
-         return $ nest { drones = insert nick drone clutch }
-    where chan   = channel nest
-          clutch = drones  nest
-          mvCmd  = command    drone
-          mvTel  = telemetry  drone
-          mvQuit = killswitch drone
+getClutch :: NestM (DroneClutch)
+getClutch = get >>= (\n -> nestIO $ return $ drones n)
+
+getChan :: NestM (Channel)
+getChan = get >>= (\n -> nestIO $ return $ channel n)
+
+addDrone :: Nick -> NestM ()
+addDrone nick
+    = do nest   <- get       
+         clutch <- getClutch
+         chan   <- getChan
+         (_, d) <- nestIO $ newDrone nick
+         mvCmd  <- nestIO . return $ command    d
+         mvTel  <- nestIO . return $ telemetry  d
+         mvQuit <- nestIO . return $ killswitch d
+
+         nestIO $ forkIO (hatchDrone nick chan (mvTel, mvCmd) mvQuit) 
+         put $ nest { drones = insert nick d clutch }
+
     
-withDrone :: Nest -> (Nick, Drone) -> IO Nest
-withDrone = flip addDrone
 
-droneFromNick :: Nest -> Nick -> Maybe Drone
-droneFromNick nest nick
-    = lookup nick clutch
-    where clutch = drones nest
+getDrone :: Nick -> NestM (Maybe Drone)
+getDrone nick
+    = do nest   <- get
+         clutch <- getClutch
+         nestIO $ return $ lookup nick clutch
 
-killDrone :: Nick -> Nest -> IO Nest
-killDrone nick nest
-    = maybe 
-        (liftIO $ return nest') 
-        (\d -> kill d >> return nest') $ mbDrone
-    where clutch  = drones nest
-          mbDrone = nest `droneFromNick` nick
-          nest'   = maybe nest (const $ nest { drones = delete nick clutch }) mbDrone
-          kill d  = do putMVar (killswitch d) True
+killDrone :: Nick -> NestM ()
+killDrone nick
+    = do nest    <- get 
+         clutch  <- getClutch 
+         mbDrone <- getDrone nick
+         nest'   <- nestIO $ return $ maybe nest (const $ nest { drones = delete nick clutch }) mbDrone 
+         put nest'
+         nestIO $ maybe 
+           (liftIO $ return ()) 
+           (\d -> kill d >> return ()) $ mbDrone
+    where kill d  = do putMVar (killswitch d) True
 
+sendByDrone :: Message -> NestM ()
+sendByDrone msg
+    = do nick    <- nestIO . return $ msgSender msg
+         mbDrone <- getDrone nick
+         nestIO $ maybe
+            (return ())
+            (\d -> putMVar (command d) msg) $ mbDrone
+
+telemetryOfDrone :: Nick -> NestM ([Message])
+telemetryOfDrone n
+    = getDrone n >>= maybe 
+            (return ([]))
+            (\d -> do mvTel <- return $ telemetry d
+                      nestIO $ isEmptyMVar mvTel >>= cond
+                            (return ([]))
+                            (takeMVar mvTel >>= (\x -> return $ Data.Foldable.toList x))
+            )
